f13bc836192854ea1247db4346db0ef016ad4253
DamodaraBarbosa <damodarabarbosa@gmail.com>
2022-07-02 17:58:55 -0300

    Testes de datascience

---
+++ testesdatabaseSP.py
@@ -0,0 +1,5 @@
+import pandas as pd
+url = "https://gist.githubusercontent.com/tgcsantos/3bdb29eba6ce391e90df2b72205ba891/raw/22fa920e80c9fa209a9fccc8b52d74cc95d1599b/dados_imoveis.csv"
+dados = pd.read_csv(url)
+
+dados.head()
